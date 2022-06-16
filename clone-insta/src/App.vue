67e7846d0cd39ec1cb79c89123e0e500c86d9188
Theo Rojas <86969945+Ziao000@users.noreply.github.com>
2022-06-16 18:59:57 +0200

    Add files via upload

---
+++ clone-insta/src/App.vue
@@ -0,0 +1,104 @@
+
+<template>
+  <div class="container">
+    <h1>InstaClone - esd</h1>
+    <div class="card-element" >
+      <div v-for="post in postList" :key="post.id">
+        <CardComponent :post="post" v-on:like="like"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CardComponent from './components/CardElement.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CardComponent
+  },
+  data() {
+    return {
+      postList: [
+        {
+          "id": "1",
+          "like": 0,
+          "name": "Jason",
+          "title": "J'adore vue.js",
+          "content": "J'adore vue.js un super Framework",
+          "imgUrl": "https://positivethinking.tech/wp-content/uploads/2021/01/Logo-Vuejs.png",
+         "comment": "Pas fan perso",
+         "commentCount": "1",
+          "commentName": "John",
+        },
+        {
+          "id": "2",
+          "like": 0,
+          "name": "Mickael",
+          "title": "LA PLAGE !",
+          "content": "Une super belle plage",
+          "imgUrl": "https://www.terre.tv/wp-content/uploads/2020/05/plus-belles-plages-cuba.jpg",
+          "comment": "<3",
+          "commentCount": "1",
+          "commentName": "John",
+        },
+        {
+          "id": "3",
+          "like": 0,
+          "name": "Stefen",
+          "title": "Une photo de mes vaccances ?",
+          "content": "Un superbe paysage",
+          "imgUrl": "https://cdn.radiofrance.fr/s3/cruiser-production/2020/06/4573d0fa-8d7c-4617-bc3b-21a159cb0f1f/870x489_vacances.jpg",
+          "comment": "J'adore",
+          "commentCount": "1",
+          "commentName": "John",
+
+        },
+        {
+          "id": "4",
+          "like": 0,
+          "name": "Thomas",
+          "title": "Hello World !",
+          "content": "Bonjour tout le monde",
+          "imgUrl": "https://i.ytimg.com/vi/cRt1LKOr2P8/maxresdefault.jpg",
+          "comment": "Super photo !",
+          "commentCount": "1",
+          "commentName": "John",
+        
+
+
+          
+        }
+      ]
+    }
+  },
+  methods: {
+    like(post) {
+      post.like++;
+    }
+  }
+}
+</script>
+
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.card-element{
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+}
+h1{
+  text-align: center;
+}
+</style>
---
+++ clone-insta/src/components/CardElement.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="card">
+        <div class="card-img">
+            <h1>{{ post.name }}</h1>
+            <img :src="post.imgUrl" alt="">
+        </div>
+        <div class="card-content">
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.content }}</p>
+        </div>
+       
+            <button v-on:click="() => this.$emit('like', post)" class="btn">
+                <span>{{ post.like }} {{ post.like > 1 ? 'Likes' : 'Like' }}</span>
+            </button>
+            <p>Commentaires {{ post.commentCount }}</p>
+            <div class="comment">
+                <div class="comment-content">
+                    <h3>{{post.commentName}}</h3>
+                    <p>{{ post.comment }}</p>
+                    <input type="text" placeholder="Ajouter un commentaires">
+                </div>
+            </div>
+     
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'CardComponent',
+    props:['post']
+}
+</script>
+
+<style scoped>
+.card {
+    align-items: center;
+   
+    width: 500px;
+    height: auto;
+    align-items: center;
+    border: rgb(73, 73, 73) 2px solid;
+    margin: 20px;
+    padding: 10px;
+}
+
+.card-img{
+    width: 100%;
+    height: 400px;
+}
+
+.card-content{
+    padding-top: 100px;
+    text-align: center;
+    
+}
+
+/* .comment-content{
+
+} */
+.card-img img{
+    width: 100%;
+    height: 100%;
+}
+
+.btn{
+    background-color: #d4008a;
+    border: none;
+    color: white;
+    padding: 20px;
+    margin: 20px;
+    cursor: pointer;
+    transition: .2s;
+}
+
+.btn:hover{
+    border-radius: 6px;
+    background-color: #fff;
+    border: #d4008a 2px solid;
+    color: #d4008a;
+    transition: .2s;
+}
+
+</style>
